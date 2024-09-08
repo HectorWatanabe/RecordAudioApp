baed006543650efed64c14538f3c6a3430133a68
Héctor Chumpitaz Watanave <70416776+HectorTrabajos@users.noreply.github.com>
2024-09-07 23:26:10 -0500

    Add Worker

--- RecordSoundApp/Program.cs
+++ RecordSoundApp/Program.cs
@@ -1,47 +1,20 @@
-﻿using Microsoft.Extensions.Configuration;
-using NAudio.Lame;
-using NAudio.Wave;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace RecordSoundApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
-            var filePath = config["ApplicationSettings:FilePath"];
-
-            if (string.IsNullOrEmpty(filePath)) throw new Exception("File Path is required.");
-
-            var fileName = $"{Guid.NewGuid()}.mp3";
-
-            var outputPath = string.Concat(filePath, "\\", fileName);
-
-            var capture = new WasapiLoopbackCapture();
-
-            using (var mp3File = new FileStream(outputPath, FileMode.Create))
+            var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
             {
-                using var writer = new LameMP3FileWriter(mp3File, capture.WaveFormat, LAMEPreset.VBR_90);
+                services.AddHostedService<Worker>();
+            })
+            .Build();
 
-                capture.DataAvailable += (s, a) =>
-                {
-                    writer.Write(a.Buffer, 0, a.BytesRecorded);
-                };
-
-                capture.StartRecording();
-
-                Console.WriteLine("Grabando... presiona Enter para detener la grabación.");
-
-                Console.ReadLine();
-
-                capture.StopRecording();
-            }
-
-            Console.WriteLine("Grabación finalizada. Archivo guardado en " + outputPath);
+            await host.RunAsync();
         }
     }
 }
---
+++ RecordSoundApp/Worker.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace RecordSoundApp
+{
+    public class Worker : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
